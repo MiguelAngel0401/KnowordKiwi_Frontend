9b6c57bc781f1cad09c62e892767f1655c1d9302
Abraham <abrahamperezquiroz0@gmail.com>
2025-06-24 12:03:44 -0600

    Inclusion_Menu_Lateral

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,22 @@
-export default function page() {
+import { Home, Compass, Users, Bookmark, Settings } from 'lucide-react';
+
+export default function Page() {
   return (
     <div>
-      <h1>Landing Page</h1>
+      <h1 className="text-white text-2xl mb-4">Landing Page</h1>
+
+      <div className="text-white">
+        <ol className="space-y-4">
+          <li className="flex items-center gap-2"><Home size={20}/>Inicio</li>
+          <li className="flex items-center gap-2"><Compass size={20} />Explorar</li>
+          <li className="flex items-center gap-2"><Users size={20} />Comunidades</li>
+          <li className="flex items-center gap-2"><Bookmark size={20} />Cursos</li>
+          <li className="flex items-center gap-2">
+            <Settings size={20} />
+            Configuración
+          </li>
+        </ol>
+      </div>
     </div>
   );
 }

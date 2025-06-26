c05ef4b5e96f2239523e8e3f513ea3e5cc94992b
Mikee <islasmiguel737@gmail.com>
2025-06-26 16:41:44 -0600

    Feat Agregando Banner y Page para perfil del usuarios

---
+++ app/profile/page.tsx
@@ -0,0 +1,20 @@
+//import { Avatar } from "@/components/ui/userProfile/Avatar";
+import { Banner } from "./[userId]/components/Banner";
+//import { Banner } from "./components/Banner";
+//import { Navbar } from "@/components/ui/navbar/Navbar";
+//import NavbarCopy from "@/components/ui/navbar/NavbarCopy";
+//import LateralMenu from "@/components/shared/LateralMenu";
+
+export default function ProfilePage() {
+  return (
+    //<div>
+      //<NavbarCopy />
+      //<div className="flex">
+        //<LateralMenu />
+        <div>
+            <Banner />
+        </div>
+      //</div>
+    //</div>
+  );
+}
